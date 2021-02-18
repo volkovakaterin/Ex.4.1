8299c5922742eb70f099e08dc7bb6445c9001d63
Ekaterina Volkova <volkovakaterin@gmail.com>
2021-02-18 23:44:22 +0400

    ESLint taken into account

--- src/js/__tests__/health.test.js
+++ src/js/__tests__/health.test.js
@@ -1,18 +1,17 @@
 import health from '../health';
 
-test ('health', ()=>{
-    expect(health({name: 'Маг', health: 90})).toBe("healthy");
+test('health', () => {
+  expect(health({ name: 'Маг', health: 90 })).toBe('healthy');
 });
-test ('health 1', ()=>{
-    expect(health({name: 'Маг', health: 50})).toBe("wounded");
+test('health 1', () => {
+  expect(health({ name: 'Маг', health: 50 })).toBe('wounded');
 });
-test ('health 2', ()=>{
-    expect(health({name: 'Маг', health: 45})).toBe("wounded");
+test('health 2', () => {
+  expect(health({ name: 'Маг', health: 45 })).toBe('wounded');
 });
-test ('health 3', ()=>{
-    expect(health({name: 'Маг', health: 15})).toBe("wounded");
+test('health 3', () => {
+  expect(health({ name: 'Маг', health: 15 })).toBe('wounded');
 });
-test ('health 4', ()=>{
-    expect(health({name: 'Маг', health: 5})).toBe("critical");
+test('health 4', () => {
+  expect(health({ name: 'Маг', health: 5 })).toBe('critical');
 });
-
--- src/js/health.js
+++ src/js/health.js
@@ -1,7 +1,4 @@
-export default function health(data){
-    if(data.health > 50) {
-        return "healthy";
-    } else if(data.health <= 50 && data.health >= 15) {
-        return "wounded";
-    } else {return "critical"}
-    }
+export default function health(data) {
+  if (data.health > 50) { return 'healthy'}
+  if (data.health <= 50 && data.health >= 15) { return 'wounded'}  
+  return 'critical' }
